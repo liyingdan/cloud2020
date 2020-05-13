37ae3ec357eda0c1da280ab52ed5ad972a99c2ab
liyingdan <lyd1511777@163.com>
2020-05-13 20:44:55 +0800

    SpringCloud Config

---
+++ cloud-config-center-3344/src/main/java/com/janet/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.janet.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Date 2020/5/12
+ * @Author Janet
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/janet/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,18 @@
+package com.janet.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Date 2020/5/12
+ * @Author Janet
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
