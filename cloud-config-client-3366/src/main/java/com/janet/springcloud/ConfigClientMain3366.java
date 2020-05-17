d63272e7cdd89d18e8951e0bb23d888f01e6a408
liyingdan <lyd1511777@163.com>
2020-05-18 00:12:03 +0800

    SpringCloud Bus

---
+++ cloud-config-client-3366/src/main/java/com/janet/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,18 @@
+package com.janet.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Description TODO
+ * @Date 2020/5/17 17:54
+ * @Author Janet
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/janet/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,27 @@
+package com.janet.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description TODO
+ * @Date 2020/5/17 17:56
+ * @Author Janet
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return "serverPort:"+serverPort+"\t\n\n configInfo:"+configInfo;
+    }
+
+}
