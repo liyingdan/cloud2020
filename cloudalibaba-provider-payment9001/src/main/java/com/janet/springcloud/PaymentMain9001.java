caadaecbcfa4722b1824df50b7e63a158be6e745
liyingdan <lyd1511777@163.com>
2020-07-29 23:49:07 +0800

    SpringAlibaba Nacos 部分

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/janet/springcloud/PaymentMain9001.java
@@ -0,0 +1,18 @@
+package com.janet.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Description TODO
+ * @Author Janet
+ * @Date 2020/7/29
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
---
+++ cloudalibaba-provider-payment9001/src/main/java/com/janet/springcloud/controller/PaymentController.java
@@ -0,0 +1,22 @@
+package com.janet.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description Nacos 注册中心 controller 类
+ * @Author Janet
+ * @Date 2020/7/29
+ */
+@RestController
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id) {
+        return "nacos registry, serverPort: "+ serverPort+"\t id"+id;
+    }
+}
